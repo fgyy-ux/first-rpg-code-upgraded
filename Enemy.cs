1acea990468376e74d1fa036d3cdbc392c50192e
fgyy-ux <arturcondratov@gmail.com>
2026-04-22 23:15:28 +0300

    Added enemy class (unfinished), and removed encription from player

---
+++ Enemy.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class Eenemy : KinematicBody2D
+
+{
+    [Export] public int maxHP = 50;
+    private int currentHP;
+
+
+    // Создаём противника, придаём значения здоровия и догруппу противников
+    public override void _Ready()
+    {
+        currentHP = maxHP;
+        AddToGrup("enemies");
+        GD.Print("Enemy spwned. HP:" + currentHP);
+    }
+}
