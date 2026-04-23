fca35ad3498e159efd383c488c3051f82f650388
fgyy-ux <arturcondratov@gmail.com>
2026-04-23 21:39:36 +0300

    Update enemy setup

--- Enemy.cs
+++ Enemy.cs
@@ -8,11 +8,25 @@ public class Enemy : KinematicBody2D
     private int currentHP;
 
 
-    // Создаём противника, придаём значения здоровия и догруппу противников
+    // Создаём противника, придаём значения здоровия и добавляем в группу противников
     public override void _Ready()
     {
         currentHP = maxHP;
-        AddToGrup("enemies");
+        AddToGroup("enemies");
         GD.Print("Enemy spwned. HP:" + currentHP);
     }
+    public void takeDmg (int amount)  // Даём противнику способность терять здоровие, когда он принимает урон
+    {
+        currentHP = currentHP - amount;
+        GD.Print("Enemy HP:" = currentHP);
+        if (currentHP <= 0);
+        Die();
+    }
+    // Создаём функцию смерти и удаление противника со сцены после смерти (безопасно удаляется, без вреда к окружению, с помощью QueueFree()
+    private void Die()
+    {
+        GD.Print("Enemy died.");
+        QueueFree();
+    }
+    
 }
